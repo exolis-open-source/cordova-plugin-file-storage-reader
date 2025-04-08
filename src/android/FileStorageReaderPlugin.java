35d527d59b65e2026fe1012bf8190248196a3e80
PIerre CLAUDON <pierre.claudon@exolis.fr>
2025-04-08 12:00:37 +0200

    Attempt1 : open invisible webview

--- src/android/FileStorageReaderPlugin.java
+++ src/android/FileStorageReaderPlugin.java
@@ -8,6 +8,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Build.VERSION;
+import android.webkit.*;
+import android.view.*;
+import android.widget.*;
 
 /**
  * This class reads local storage and send it to cordova.
@@ -17,16 +20,41 @@ public class FileStorageReaderPlugin extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if (action.equals("readFileStorage")) {
-      this.readFileStorage(callbackContext);
+      this.readFileStorage(args.getString(0), callbackContext);
       return true;
     }
     return false;
   }
 
-  private void readFileStorage(CallbackContext callbackContext) {
-    if (VERSION.SDK_INT >= 21) {
-      callbackContext.success();
-    }
-    callbackContext.success();
+  private void readFileStorage(String url, CallbackContext callbackContext) {
+    WebView myWebView = new WebView(cordova.getContext());
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setAllowFileAccess(true);
+    webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+    webView.setVisibility(View.INVISIBLE);
+
+    webView.addJavascriptInterface(new Object() {
+      @JavascriptInterface
+      public void onData(String json) {
+        callbackContext.success(json);
+      }
+    }, "JSBridge");
+
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        view.evaluateJavascript(
+          "(function() {" +
+          " var out = {};" +
+          " for (var i = 0; i < localStorage.length; i++) {" +
+          "   var k = localStorage.key(i);" +
+          "   out[k] = localStorage.getItem(k);" +
+          " }" +
+          " JSBridge.onData(JSON.stringify(out));" +
+          "})()", null);
+      }
+    });
+    cordova.getActivity().addContentView(webView, new FrameLayout.LayoutParams(1, 1));
+    webView.loadUrl(url);
   }
 }
